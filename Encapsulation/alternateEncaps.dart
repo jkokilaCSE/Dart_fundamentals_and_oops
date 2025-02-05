129bc7037b933fc7b79c3518803a4a0d7947b727
Kokila <jkokila2810@gmail.com>
2025-02-05 23:04:49 +0530

    updated dart concepts

--- Encapsulation/alternateEncaps.dart
+++ Encapsulation/alternateEncaps.dart
@@ -1,22 +1,30 @@
 class Student{
 
-  String? _name;
+  String? _name="s";
   int ? _age;
 
 
-  set name(String name)=> _name = name;
+  set name(String name){
+    printing();
+  }
   
   String get name=>_name!;//read only
 
   set age(int age)=>_age=age;//modify
 
   int get age=>_age!;
-
+  
+  printing(){
+  print(age);
+  }
 
 }
 
 void main(){
   Student s =Student();
-  s.name="koki";
   s.age=22;
+  s.name="koki";
+  
+  //s.name = s.age.toString();
+  print(s.name);
 }
